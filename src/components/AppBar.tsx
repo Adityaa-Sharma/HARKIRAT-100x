9e70104b1ebf553a0332fb95e3a9fde9423ab61c
Aditya Sharma <aditya@pgagi.in>
2024-09-16 03:03:20 +0530

    search bar is added

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,17 @@
+import { SearchBar } from "./SearchBar"
+
+export const AppBar=()=>{
+    return (
+        <div className="flex justify-between pt-1 p-3">
+            <div>
+                Youtube
+            </div>
+            <div>
+                <SearchBar/>
+            </div>
+            <div>
+                Sign in
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
 import Image from "next/image";
 import localFont from "next/font/local";
 import {VideoCard} from "../components/VideoCard";
+import { VideoGrid } from "../components/VideoGrid";
+import {AppBar} from "../components/AppBar";
 
 export default function Home() {
   return (
     <div>
-      <VideoCard title="India's got latent"
-      image="photo.jpg"
-      thumb="samay.jpg"
-      channel="Samay Raina"
-      views="46Mn"
-      timestamp="4 days ago">
-      </VideoCard>
+      <AppBar/>
+      <VideoGrid/>
 
     </div>
   );
